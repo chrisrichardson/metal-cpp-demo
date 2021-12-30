aa3febf7dfa6792b7998ee4fab9dcdca06121a74
Hof, Moritz Travis <moritz.hof@dlr.de>
2021-12-30 22:10:40 +0100

    Initial Commit

---
+++ try/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  try
+//
+//  Created by Moritz Hof on 30.12.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
